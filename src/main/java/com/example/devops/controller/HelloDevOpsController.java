2de0f2b64ff2f43b644de9067d61b59c3754c7fc
Paul <xajingcai@gmail.com>
2018-12-11 22:56:20 +0800

    test modify

--- src/main/java/com/example/devops/controller/HelloDevOpsController.java
+++ src/main/java/com/example/devops/controller/HelloDevOpsController.java
@@ -3,16 +3,11 @@ package com.example.devops.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Spring Boot HelloWorld案例
- *
- * Created by bysocket on 16/4/26.
- */
 @RestController
-public class HelloWorldController {
+public class HelloDevOpsController {
 
-    @RequestMapping("/")
+    @RequestMapping("/devops")
     public String sayHello() {
-        return "Hello,World!!";
+        return "Hello DevOps!";
     }
 }
